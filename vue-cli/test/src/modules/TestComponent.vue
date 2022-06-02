d7f81fa37c746fc31b5775fbd6af66f8152d3849
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-06-02 23:38:42 +0900

    簡単な component 作成

---
+++ vue-cli/test/src/modules/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Testです
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'TestComponent',
+	data() {
+		return {
+			testData: 'Test data です'
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
