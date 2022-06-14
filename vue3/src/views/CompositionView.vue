cab55bc27f7bea7c2eb3831ca1eff65d3ca302fe
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-06-14 23:01:51 +0900

    setup 関数内の各種メソッド

--- vue3/src/views/CompositionView.vue
+++ vue3/src/views/CompositionView.vue
@@ -6,15 +6,23 @@
 	<p>nameRef: {{ nameRef }}</p>
 	<p>book.title: {{ book.title }}</p>
 	<p>book.author: {{ book.author[0] }}</p>
+	<button @click="btnClick">Click</button>
+	<p>computed: {{ totalPrice }}</p>
+	<div>watch:
+		<input type="text" v-model="search"/>
+	</div>
+	<div>watchEffect:
+		<input type="text" v-model="searchEffect"/>
+	</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed, watch, watchEffect, onMounted } from 'vue'
 export default {
 	data() {},
 	setup() {
-		// console.log('setup');
+		console.log('setup');
 		const name = '大谷'
 		const age = 30
 
@@ -30,11 +38,50 @@ export default {
 		})
 		console.table(book);
 
+		// setup 関数内でメソッドを定義
+		const btnClick = (e) => {
+			console.log(book.title);
+			console.log(e);
+		}
+
+		// computed リアルタイムで変更したい内容
+		const item = reactive({
+			price: 100,
+			number: 1
+		})
+		const totalPrice = computed(() => {
+			return item.price * item.number
+		});
+
+		// watch -> 引数２個
+		const search = ref('')
+		watch(search, (newVal, oldVal) => {
+			console.log(`watch: ${search.value}`);
+			console.log(`new: ${newVal}`);
+			console.log(`old: ${oldVal}`);
+		})
+
+		// watchEffect -> 引数1個
+		const searchEffect = ref('')
+		watchEffect(() => {
+			console.log(`watchEffect: ${searchEffect.value}`);
+		})
+
+		// onMounted -> 少し遅れて実行される
+		onMounted(() => {
+			console.log('onMounted');
+		})
+
 		return {
 			name,
 			age,
 			nameRef,
 			book,
+			btnClick,
+			item,
+			totalPrice,
+			search,
+			searchEffect,
 		}
 	},
 	created() {
