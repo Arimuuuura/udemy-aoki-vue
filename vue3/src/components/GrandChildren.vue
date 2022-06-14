a210be1ac245650eee8f0c2c2fc9cd0d02eaa773
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-06-14 13:40:59 +0900

    子、孫のprops作成

---
+++ vue3/src/components/GrandChildren.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+	GrandChildren
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue3/src/views/ChildrenView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+	Children
+	<GrandChildren />
+  </div>
+</template>
+
+<script>
+import GrandChildren from '@/components/GrandChildren'
+export default {
+	components: {
+		GrandChildren
+	}
+}
+</script>
+
+<style>
+
+</style>
