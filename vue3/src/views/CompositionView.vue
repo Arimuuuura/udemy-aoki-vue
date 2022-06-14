98509f1cedfd6566915441937b46f1d37e4c27d2
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-06-14 14:27:59 +0900

    ref, reactive の扱い

--- vue3/src/views/CompositionView.vue
+++ vue3/src/views/CompositionView.vue
@@ -3,10 +3,14 @@
 	CompositionView
 	<p>name: {{ name }}</p>
 	<p>age: {{ age }}</p>
+	<p>nameRef: {{ nameRef }}</p>
+	<p>book.title: {{ book.title }}</p>
+	<p>book.author: {{ book.author[0] }}</p>
   </div>
 </template>
 
 <script>
+import { ref, reactive } from 'vue'
 export default {
 	data() {},
 	setup() {
@@ -14,9 +18,23 @@ export default {
 		const name = '大谷'
 		const age = 30
 
+		// ref
+		const nameRef = ref('有村');
+		// script 内で扱う場合は .value で繋ぐ
+		console.log(nameRef.value);
+
+		// reactive
+		const book = reactive({
+			title: '本のタイトル',
+			author: ['山田', '坂本']
+		})
+		console.table(book);
+
 		return {
 			name,
-			age
+			age,
+			nameRef,
+			book,
 		}
 	},
 	created() {
