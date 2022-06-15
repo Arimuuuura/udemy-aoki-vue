88b3515ff92ea453c942d581fb0201d04fec03aa
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-06-15 22:29:19 +0900

    setup 内でナビゲーションガード

--- vue3/src/views/RouterTest.vue
+++ vue3/src/views/RouterTest.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { useRouter, useRoute } from 'vue-router'
+import { useRouter, useRoute, onBeforeRouteLeave } from 'vue-router'
 export default {
 	setup() {
 		const router = useRouter();
@@ -20,6 +20,11 @@ export default {
 		const checkRoutePath = () => {
 			console.log(route.path);
 		}
+
+		onBeforeRouteLeave((to, from) => {
+			console.log(`to: ${to}`);
+			console.log(`from: ${from}`);
+		});
 
 		return {
 			goHome,
